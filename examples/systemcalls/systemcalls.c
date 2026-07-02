1ae567c8d7c2b253251719d0967cdb7d7fc66a6e
KHa321Z <khammadzaeem@gmail.com>
2026-07-02 23:31:28 +0500

    Part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,6 +16,10 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+	int response = system(cmd);
+	
+	if (response == -1)
+		return false;
 
     return true;
 }
@@ -47,7 +51,7 @@ bool do_exec(int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    // command[count] = command[count];
 
 /*
  * TODO:
@@ -58,6 +62,20 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+	
+	int status;
+	pid_t pid = fork();
+	
+	if (pid == -1)
+		return false;
+	
+	else if (pid == 0) {
+		execv(command[0], command);
+		exit(EXIT_FAILURE);
+	}
+	
+	if ((waitpid(pid, &status, 0) == -1) || (WIFEXITED(status) == false) || (WEXITSTATUS(status) != 0))
+		return false;
 
     va_end(args);
 
@@ -82,7 +100,7 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    // command[count] = command[count];
 
 
 /*
@@ -92,6 +110,34 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+
+	int status;
+	int fd = open(outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	
+	if (fd == -1)
+		return false;
+		
+	pid_t pid = fork();
+	
+	if (pid == -1) {
+		close(fd);
+		return false;
+	}
+	
+	else if (pid == 0) {
+		if (dup2(fd, 1) == -1) {
+			close(fd);
+			return false;
+		}
+	
+		execv(command[0], command);
+		exit(EXIT_FAILURE);
+	}
+	
+	close(fd);
+	
+	if ((waitpid(pid, &status, 0) == -1) || (WIFEXITED(status) == false) || (WEXITSTATUS(status) != 0))
+		return false;
 
     va_end(args);
 
